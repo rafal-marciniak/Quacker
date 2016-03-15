144fc847c71f65393fb908229d402e46f394d73b
rafal-marciniak <rafal.marciniak@gmail.com>
2016-03-15 16:06:26 +0100

    Update quackerDirectives.js

--- Quacker/Scripts/quackerDirectives.js
+++ Quacker/Scripts/quackerDirectives.js
@@ -1,19 +1,24 @@
-﻿(function(){
+(function(){
 	angular.module('quackerDirectives', [])
+
 		.directive('quackForm', function () {
 			return {
 				restrict: 'E',
 				scope: {
 					labelText: '@',
-					parentQuackId: '=',
+					parentQuackId: '@',
 					onPost: '&'
 				},
 				templateUrl: '/Template/QuackForm',
 				controller: ['$scope', function ($scope) {
-					$scope.quack = { ParentQuackId: $scope.parentQuackId };
+					$scope.createQuack = function () {
+						$scope.quack = { ParentQuackId: $scope.parentQuackId };
+					};
+
 					$scope.formName = 'quackForm' + $scope.parentQuackId;
+					$scope.createQuack();
 					$scope.onPostCallback = function () {
-						$scope.quack = {};
+						$scope.createQuack();
 					};
 				}]
 			};
